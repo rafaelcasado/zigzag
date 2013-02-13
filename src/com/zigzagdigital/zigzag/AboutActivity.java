0f296099acacbc669180ffdd33124be9b658b601
rafaelcasado <rafael.casado@yahoo.es>
2013-02-13 19:58:03 +0100

    Añadido botón about funcional

---
+++ src/com/zigzagdigital/zigzag/AboutActivity.java
@@ -0,0 +1,14 @@
+package com.zigzagdigital.zigzag;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+	}
+}
--- src/com/zigzagdigital/zigzag/ArticleList.java
+++ src/com/zigzagdigital/zigzag/ArticleList.java
@@ -1,15 +1,34 @@
 package com.zigzagdigital.zigzag;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class ArticleList extends Activity {
+public class ArticleList extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_article_list);
+		Button botonAbout = (Button) findViewById(R.id.about_boton);
+		botonAbout.setOnClickListener(this);
 	}
 
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+		switch (v.getId()) {
+		case R.id.about_boton:
+			Context context = this;
+			Intent intent = new Intent (context, AboutActivity.class);
+			startActivity(intent);
+			break;
+		}
+	}
 }
